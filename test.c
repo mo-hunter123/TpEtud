c1071ad515369cbedb14793fd2dc54800cf217a9
Mohamed AHRRASS <mohamed.ahrrass@edu.uca.ma>
2021-03-05 20:21:13 +0000

    tested gitpod

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 #define MAX_ET 11
-#define MAX_MOD 9
+#define MAX_MOD 13
 
 //take words of a text and store them on a data structure 
 
@@ -22,13 +22,30 @@
 //11 => Eco
 
 
+#define ModulesAnnee[][] = 
+    {
+        "Algo", 
+        "Sdd", 
+        "BD", 
+        "SE", 
+        "Archi", 
+        "TECH", 
+        "Reseaux", 
+        "Web", 
+        "Conce", 
+        "UML", 
+        "Droit", 
+        "Eco"
+    };
+
+
 typedef struct Cel{
     float point;
     Etudiant *INfos;
     struct Cel *svt;
 }Note;
 
-//Note *notesEt[13];
+//Note *notesEt[13]; 13: nombre des modules 
 
 typedef struct Inf{
     char CNE[11];
@@ -38,7 +55,7 @@ typedef struct Inf{
     struct Inf *next;
 }Etudiant;
 
-//Etudiant* ets[26][10]
+//Etudiant* ets[26][10] 26: nombre des lettres, 10 cas pour le dernier caractere de cne
 
 typedef struct NP{
     char Nom[20];
@@ -47,15 +64,48 @@ typedef struct NP{
     struct NP *next;
 }NomEtu;
 
-//NomEtu* nomsets[26]
+//NomEtu* nomsets[26] 26: nombre des cas pour la premiere lettre d'un nom 
 
-int InitEnsemble(Etudiant *MaTable[MAX_ET]){
-    for(int i = 0; i < MAX_ET; i++)
+int InitEnsembles(Etudiant *MaTable[26][10], NomEtu* NomSets[26], Note *NoteEt[MAX_MOD])
+{
+    int i, j;
+    //initialisation pour la base des etudiants 
+    //et base des Noms 
+    for(i = 0; i<26; i++)
     {
-        MaTable[i] = NULL;
+        NomSets[i] = NULL;
+        for(j = 0; j<10; j++)
+        {
+            MaTable[i][j] = NULL;
+        }
     }
+    //initialisation de base des Notes 
+    for(i = 0; i<MAX_MOD; i++)
+    {
+        NoteEt[i] = NULL;
+    }
+
     return ((int)1);
 }
+
+
+//NomEtu *nomsets[26]
+int Cle_Etud_cne(char Nom[20]
+{
+    return ((int) 'z'-Nom[0]);
+}
+
+int Cle_Etud(int *ind1, int *ind2, char CNE[20]){
+    if(sizeof(CNE) != 10){
+        printf("\nErr avec le CNE\n");
+        return ((int)-1);
+    }
+
+    *ind1 = 'Z' - CNE[0];
+    *ind2 = '9' - CNE[9] + 1;
+    return ((int)1);
+}
+
 
 Note* MakeNewNote(float no, char Module[20]){
     Note *NE = (Note*)malloc(sizeof(Note));
@@ -70,63 +120,127 @@ Note *AddEnd(Note* Liste, Note *NE){
     return NE;
 }
 
-Note* InsertionModules(Note *Liste, FILE *nptr)
-{
-    float no; 
-    char Module[20];
-    Note *NE = NULL;
-    for(int j = 0; j<MAX_MOD; j++)
-    {
-        fscanf(nptr, "%s", Module);
-        fscanf(nptr, "%g", &no);
-        NE = MakeNewNote(no, Module);
-        Liste = AddEnd(Liste, NE);
-    }
-    return Liste;
-}
-
-
-//so basic function for insertion of students 
-int TelechargerAuTableau(Etudiant *MaTable[MAX_ET], FILE *fptr){
-    char Etud[30];
-    for(int i = 0; i< MAX_ET; i++){
-        MaTable[i] = (Etudiant*)malloc(sizeof(Etudiant));
-        fscanf(fptr, "%s", MaTable[i]->CNE);
-        fscanf(fptr, "%s", MaTable[i]->Nom);
-        fscanf(fptr, "%s", MaTable[i]->Prenom);
-        fscanf(fptr, "%s", MaTable[i]->Date_naiss);
-
-        strcpy(Etud, "Note/");
-        strncat(Etud, MaTable[i]->CNE, sizeof(MaTable[i]->CNE));
-        strncat(Etud, ".txt", 4);
-        FILE * nptr = fopen(Etud, "r");
-       
-        MaTable[i]->modules = InsertionModules(MaTable[i]->modules, nptr);
-        
-    }
-    return ((int)1);
-}
-
-
-int AffichageTableau(Etudiant *MaTable[MAX_ET]){
-    for(int i = 0; i< MAX_ET; i++){
-        printf("%s ", MaTable[i]->CNE);
-        printf("%s ", MaTable[i]->Nom);
-        printf("%s ", MaTable[i]->Prenom);
-        printf("%s ", MaTable[i]->Date_naiss);
-        Note *crt = MaTable[i]->modules;
-        printf("\n[");
-        while(crt){
-            printf("%s: %g   ", crt->Module, crt->point);
-            crt = crt->svt;
-        }
-        printf("]");
-        printf("\n");
-    }
-    return ((int)1);
-}
 
 // int InsertionPoint()
+// insertion note 
+Note * insertionNote(Note *liste, Note *elem)
+{
+    Note *crt=NULL;
+    if(!liste) return((Note*)elem);
+    if (elem->point > liste->point)
+    {
+        elem->svt = liste;
+        return((Note *)elem); 
+    }
+    crt = liste;
+    while(crt->svt)
+    {
+        if(elem->point > crt->svt->point)
+        {
+            elem->svt = crt->svt;
+            crt->svt = elem;
+            return((Note*)liste);
+        }
+        crt = crt->svt;
+    } 
+    crt->svt = elem;
+    return((Note*)liste);
+}
+
+// insertion nom 
+NomEtu * insertionNom(NomEtu *liste, NomEtu *elem)
+{
+    NomEtu *crt=NULL;
+    if(!liste) return((NomEtu*)elem);
+    if (strcmp(elem->Nom,liste->Nom) > 0)
+    {
+        elem->next = liste;
+        return((NomEtu*)elem); 
+    }
+    crt = liste;
+    while(crt->svt)
+    {
+        if(strcmp(elem->Nom,liste->Nom) > 0)
+        {
+            elem->svt = crt->svt;
+            crt->svt = elem;
+            return((NomEtu*)liste);
+        }
+        crt = crt->svt;
+    } 
+    crt->svt = elem;
+    return((NomEtu*)liste);
+} 
+// int InsertionPoint()
+// insertion note 
+Note * insertionNote(Note *liste, Note *elem)
+{
+    Note *crt=NULL;
+    if(!liste) return((Note*)elem);
+    if (elem->point > liste->point)
+    {
+        elem->svt = liste;
+        return((Note *)elem); 
+    }
+    crt = liste;
+    while(crt->svt)
+    {
+        if(elem->point > crt->svt->point)
+        {
+            elem->svt = crt->svt;
+            crt->svt = elem;
+            return((Note*)liste);
+        }
+        crt = crt->svt;
+    } 
+    crt->svt = elem;
+    return((Note*)liste);
+}
+
+// insertion nom 
+NomEtu * insertionNom(NomEtu *liste, NomEtu *elem)
+{
+    NomEtu *crt=NULL;
+    if(!liste) return((NomEtu*)elem);
+    if (strcmp(elem->Nom,liste->Nom) > 0)
+    {
+        elem->next = liste;
+        return((NomEtu*)elem); 
+    }
+    crt = liste;
+    while(crt->svt)
+    {
+        if(strcmp(elem->Nom,liste->Nom) > 0)
+        {
+            elem->svt = crt->svt;
+            crt->svt = elem;
+            return((NomEtu*)liste);
+        }
+        crt = crt->svt;
+    } 
+    crt->svt = elem;
+    return((NomEtu*)liste);
+} 
+
+// insertion nom 
+Etudiant * insertionInf(Etudiant *liste, Etudiant *elem)
+{
+    elem->next = liste;
+    return((NomEtu*)elem); 
+} 
+ 
+
+void Affichage_ordre_merite(Note *NoteEt[MAX_MOD])
+{
+    printf("\nCNE\tNOM\tPRENOM\tMODULE\tNOTE\n");
+    
+    int i= 0;
+    for(i = 0; i<12; i++)
+    {
+        printf("%c\t%c\t%c\t%c\t%c\n", NoteEt->)
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -136,11 +250,6 @@ int main(int argc, char const *argv[])
         printf("ERR");
         exit(-1);
     }
-
-    Etudiant *Ensemble[MAX_ET];
-    InitEnsemble(Ensemble);
-    TelechargerAuTableau(Ensemble, fptr);    
-    AffichageTableau(Ensemble);
 
     fclose(fptr);
     printf("\n");
