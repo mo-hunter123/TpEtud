89e3c6b38bbd67934fec47379bd3bc695735e803
mo-hunter123 <mohamed.makromi.2000@gmail.com>
2021-03-05 21:28:37 +0100

    hi

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "hello.h"
 
 #define MAX_ET 11
 #define MAX_MOD 13
@@ -22,49 +23,8 @@
 //11 => Eco
 
 
-#define ModulesAnnee[][] = 
-    {
-        "Algo", 
-        "Sdd", 
-        "BD", 
-        "SE", 
-        "Archi", 
-        "TECH", 
-        "Reseaux", 
-        "Web", 
-        "Conce", 
-        "UML", 
-        "Droit", 
-        "Eco"
-    };
+char ModulesAnnee[13][8] =  { "Algo", "Sdd", "BD", "SE", "Archi", "TECH", "Reseaux", "Web", "Conce", "UML", "Droit", "Eco"};
 
-
-typedef struct Cel{
-    float point;
-    Etudiant *INfos;
-    struct Cel *svt;
-}Note;
-
-//Note *notesEt[13]; 13: nombre des modules 
-
-typedef struct Inf{
-    char CNE[11];
-    char Date_naiss[12];
-    Note *modules[13];
-    NomEtu *nom;
-    struct Inf *next;
-}Etudiant;
-
-//Etudiant* ets[26][10] 26: nombre des lettres, 10 cas pour le dernier caractere de cne
-
-typedef struct NP{
-    char Nom[20];
-    char Prenom[20];
-    Etudiant *infoEtu;
-    struct NP *next;
-}NomEtu;
-
-//NomEtu* nomsets[26] 26: nombre des cas pour la premiere lettre d'un nom 
 
 int InitEnsembles(Etudiant *MaTable[26][10], NomEtu* NomSets[26], Note *NoteEt[MAX_MOD])
 {
@@ -90,7 +50,7 @@ int InitEnsembles(Etudiant *MaTable[26][10], NomEtu* NomSets[26], Note *NoteEt[M
 
 
 //NomEtu *nomsets[26]
-int Cle_Etud_cne(char Nom[20]
+int Cle_Etud_cne(char Nom[20])
 {
     return ((int) 'z'-Nom[0]);
 }
@@ -106,14 +66,6 @@ int Cle_Etud(int *ind1, int *ind2, char CNE[20]){
     return ((int)1);
 }
 
-
-Note* MakeNewNote(float no, char Module[20]){
-    Note *NE = (Note*)malloc(sizeof(Note));
-    strcpy(NE->Module, Module);
-    NE->point = no;
-    NE->svt = NULL;
-    return ((Note*)NE);
-}
 
 Note *AddEnd(Note* Liste, Note *NE){
     NE->svt = Liste; 
@@ -154,57 +106,7 @@ NomEtu * insertionNom(NomEtu *liste, NomEtu *elem)
     if(!liste) return((NomEtu*)elem);
     if (strcmp(elem->Nom,liste->Nom) > 0)
     {
-        elem->next = liste;
-        return((NomEtu*)elem); 
-    }
-    crt = liste;
-    while(crt->svt)
-    {
-        if(strcmp(elem->Nom,liste->Nom) > 0)
-        {
-            elem->svt = crt->svt;
-            crt->svt = elem;
-            return((NomEtu*)liste);
-        }
-        crt = crt->svt;
-    } 
-    crt->svt = elem;
-    return((NomEtu*)liste);
-} 
-// int InsertionPoint()
-// insertion note 
-Note * insertionNote(Note *liste, Note *elem)
-{
-    Note *crt=NULL;
-    if(!liste) return((Note*)elem);
-    if (elem->point > liste->point)
-    {
         elem->svt = liste;
-        return((Note *)elem); 
-    }
-    crt = liste;
-    while(crt->svt)
-    {
-        if(elem->point > crt->svt->point)
-        {
-            elem->svt = crt->svt;
-            crt->svt = elem;
-            return((Note*)liste);
-        }
-        crt = crt->svt;
-    } 
-    crt->svt = elem;
-    return((Note*)liste);
-}
-
-// insertion nom 
-NomEtu * insertionNom(NomEtu *liste, NomEtu *elem)
-{
-    NomEtu *crt=NULL;
-    if(!liste) return((NomEtu*)elem);
-    if (strcmp(elem->Nom,liste->Nom) > 0)
-    {
-        elem->next = liste;
         return((NomEtu*)elem); 
     }
     crt = liste;
@@ -237,7 +139,7 @@ void Affichage_ordre_merite(Note *NoteEt[MAX_MOD])
     int i= 0;
     for(i = 0; i<12; i++)
     {
-        printf("%c\t%c\t%c\t%c\t%c\n", NoteEt->)
+        printf("%c\t%c\t%c\t%c\t%c\n");
     }
 }
 
---
+++ hello.h
@@ -0,0 +1,27 @@
+
+typedef struct Cel{
+    float point;
+    struct Inf *INfos;
+    struct Cel *svt;
+}Note;
+
+//Note *notesEt[13]; 13: nombre des modules 
+
+typedef struct Inf{
+    char CNE[11];
+    char Date_naiss[12];
+    Note *modules[13];
+    struct NP *nom;
+    struct Inf *next;
+}Etudiant;
+
+//Etudiant* ets[26][10] 26: nombre des lettres, 10 cas pour le dernier caractere de cne
+
+typedef struct NP{
+    char Nom[20];
+    char Prenom[20];
+    Etudiant *infoEtu;
+    struct NP *svt;
+}NomEtu;
+
+//NomEtu* nomsets[26] 26: nombre des cas pour la premiere lettre d'un nom 
